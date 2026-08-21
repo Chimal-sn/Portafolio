f95eb651248862caafdb6d29040606734bf948c3
Sony <sonychimo10@hotmail.com>
2026-08-21 17:44:27 -0600

    Ft. Prueba de navbar

--- js/animaciones.js
+++ js/animaciones.js
@@ -7,13 +7,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
     tl.from(izquierda_fondo, {
         xPercent: -100,
-        duration: 1.5,
+        duration: 1,
         ease: "power3.inOut"
     }).from(derecha_fondo, {
         xPercent: 100,
-        duration: 1.5,
+        duration: 1,
         ease: "power3.inOut"
     }, "<").set(fondoNegro, {
         display: "none"
     })
+
+
+    const flecha_nav = document.getElementById("flecha_nav");
+    const header_pagina = document.getElementById("header_pagina");
+    const menuTimeline = gsap.timeline({ paused: true, reversed: true });
+    menuTimeline
+        .to(header_pagina, {
+            xPercent: 85,
+            duration: 0.6,
+            ease: "power3.inOut"
+        })
+        .to(flecha_nav, {
+            rotate: 180,
+            duration: 0.6,
+            ease: "power3.inOut"
+        }, "<");
+
+    flecha_nav.addEventListener("click", () => {
+        menuTimeline.reversed() ? menuTimeline.play() : menuTimeline.reverse();
+    });
 });
